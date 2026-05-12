8d568853b72f0964c9107938cef6d52660042969
mostafa <mostafagholamnataj@gmail.com>
2026-05-12 14:27:18 +0330

    end of s4-2

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,21 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
+# config/asgi.py
 import os
-
+import django
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+django.setup()
 
-application = get_asgi_application()
+from apps.realtime.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    # Standard HTTP requests → Django handles as normal
+    "http": get_asgi_application(),
+
+    # WebSocket requests → routed to our consumers
+    "websocket": AllowedHostsOriginValidator(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
